d185c72337c7cd04e8330027776440493e625d11
Serhiy Yarosh <m7grey@gmail.com>
2018-11-01 13:17:30 -0400

    Lecture 78 - Timezones. Lecture 82 - Using Timezones

--- Section11/Lecture 82 - Using Timezones/dateandtime.py
+++ Section11/Lecture 82 - Using Timezones/dateandtime.py
@@ -5,12 +5,14 @@
 # print("The current timezone is {0} with an offset of {1}".format(time.tzname[0], time.timezone))
 #
 # if time.daylight != 0:
-#     print("\tDaylight Saving Time (DST) is defined for this location")
-#     print("\tThe DST timezone name is " + time.tzname[1])
+#     print("\tDaylight Saving Time is in effect for this location")
+#     print("\tThe DST timezone is " + time.tzname[1])
 #
 # print("Local time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-# print("UTC is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
+# print("UTC time is " + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
+#
 import datetime
+
 
 print(datetime.datetime.today())
 print(datetime.datetime.now())
