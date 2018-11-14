eeb5e6ec89ed3e4fb4078881d2547af7c980fb6b
Serhiy Yarosh <m7grey@gmail.com>
2018-11-14 13:22:25 -0500

    Lecture 177 - Scrollbars- Format.

--- Section13/Lecture 176 - Simple Database Browser/jukebox.py
+++ Section13/Lecture 176 - Simple Database Browser/jukebox.py
@@ -1,10 +1,3 @@
-# import sqlite3
-#
-# conn = sqlite3.connect('music.sqlite')
-#
-# for x in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", (196,)):
-#     print(x)
-
 import sqlite3
 try:
     import tkinter
