c61a27cb9807644837b96c86bfc6fad266257997
Serhiy Yarosh <m7grey@gmail.com>
2018-11-14 14:13:40 -0500

    Lecture 181 - Scrollable ListBox. class Scrollbox - v2.

--- Section13/Lecture 176 - Simple Database Browser/jukebox.py
+++ Section13/Lecture 176 - Simple Database Browser/jukebox.py
@@ -29,7 +29,7 @@ mainWindow.geometry('1024x768')
 mainWindow.columnconfigure(0, weight=2)
 mainWindow.columnconfigure(1, weight=2)
 mainWindow.columnconfigure(2, weight=2)
-mainWindow.columnconfigure(3, weight=1)  # spacer column on right
+mainWindow.columnconfigure(3, weight=1)    # spacer column on right
 
 mainWindow.rowconfigure(0, weight=1)
 mainWindow.rowconfigure(1, weight=5)
@@ -42,29 +42,21 @@ tkinter.Label(mainWindow, text="Albums").grid(row=0, column=1)
 tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
 # ===== Artists Listbox =====
-artistList = tkinter.Listbox(mainWindow)
+artistList = Scrollbox(mainWindow)
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
-
-artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
-artistScroll.grid(row=1, column=0, sticky='nse', rowspan=2)
-artistList['yscrollcommand'] = artistScroll.set
 
 # ===== Albums Listbox =====
 albumLV = tkinter.Variable(mainWindow)
 albumLV.set(("Choose an artist",))
-albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
+albumList = Scrollbox(mainWindow, listvariable=albumLV)
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
-
-albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
-albumScroll.grid(row=1, column=1, sticky='nse', rowspan=2)
-albumList['yscrollcommand'] = albumScroll.set
 
 # ===== Songs Listbox =====
 songLV = tkinter.Variable(mainWindow)
 songLV.set(("Choose an album",))
-songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+songList = Scrollbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 
