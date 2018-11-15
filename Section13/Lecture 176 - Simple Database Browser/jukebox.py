f92480936d608f2e3b05e3484a153dff05a666a0
Serhiy Yarosh <m7grey@gmail.com>
2018-11-15 12:06:42 -0500

    Lecture 187 - DataListbox-Challenge.

--- Section13/Lecture 176 - Simple Database Browser/jukebox.py
+++ Section13/Lecture 176 - Simple Database Browser/jukebox.py
@@ -28,6 +28,7 @@ class DataListBox(Scrollbox):
 
         self.linked_box = None
         self.link_field = None
+        self.link_value = None
 
         self.cursor = connection.cursor()
         self.table = table
@@ -49,6 +50,7 @@ class DataListBox(Scrollbox):
         widget.link_field = link_field
 
     def requery(self, link_value=None):
+        self.link_value = link_value    # store the id, so we know the "master" record we're populated from
         if link_value and self.link_field:
             sql = self.sql_select + " WHERE " + self.link_field + "=?" + self.sql_sort
             print(sql)      # TODO delete this line
@@ -71,8 +73,15 @@ class DataListBox(Scrollbox):
             index = self.curselection()[0]
             value = self.get(index),
 
-            # get the artist ID from the database row
-            link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]
+            # get the ID from the database row
+            # Make sure we're getting the correct one, by including the link_value if appropriate
+            if self.link_value:
+                value = value[0], self.link_value
+                sql_where = " WHERE " + self.field + "=? AND " + self.link_field + "=?"
+            else:
+                sql_where = " WHERE " + self.field + "=?"
+
+            link_id = self.cursor.execute(self.sql_select + sql_where, value).fetchone()[1]
             self.linked_box.requery(link_id)
 
 
