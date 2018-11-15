d705c4b0eb72db64c2b4a4dda89bfcd358bcc0de
Serhiy Yarosh <m7grey@gmail.com>
2018-11-15 18:28:53 -0500

    204-Nested-Comprehensions- Challenge - Solution.

--- Section14/204-Nested-Comprehensions/nested_challenge.py
+++ Section14/204-Nested-Comprehensions/nested_challenge.py
@@ -7,3 +7,15 @@
 for i in range(1, 11):
     for j in range(1, 11):
         print(i, i * j)
+
+times = [(i, i * j) for i in range(1, 11) for j in range(1, 11)]
+print(times)
+
+for x, y in [(i, i * j) for i in range(1, 11) for j in range(1, 11)]:
+    print(x, y)
+
+times2 = [[(i, i * j) for i in range(1, 11)] for j in range(1, 11)]
+print(times2)
+
+for x, y in ((i, i * j) for i in range(1, 11) for j in range(1, 11)):
+    print(x, y)
