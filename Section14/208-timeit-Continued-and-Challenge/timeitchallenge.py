4c9348156f28e2f4a4ac2bf51fbd311a5d120921
Serhiy Yarosh <m7grey@gmail.com>
2018-12-09 12:56:04 -0500

    208- timeitchallenge. Recursive is twice longer.

--- Section14/208-timeit-Continued-and-Challenge/timeitchallenge.py
+++ Section14/208-timeit-Continued-and-Challenge/timeitchallenge.py
@@ -9,8 +9,7 @@
 # of one million will take a long time to run.
 
 import timeit
-
-
+fact_test = """\
 def fact(n):
     result = 1
     if n > 1:
@@ -18,10 +17,22 @@ def fact(n):
             result *= f
     return result
 
+x = fact(130)
+"""
 
+factorial_test = """\
 def factorial(n):
     # n! can also be defined as n * (n-1)!
     if n <= 1:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
+
+
+
+
+y = factorial(130)
+"""
+
+print(timeit.timeit(fact_test, number=10000))
+print(timeit.timeit(factorial_test, number=10000))
